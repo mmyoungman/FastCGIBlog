fcc9cfde553207beee5a2ce6e60d29e639415cc8
Mark Youngman <mmyoungman@gmail.com>
2017-09-07 07:36:52 +0100

    Changed ec2-startup.sh to copy over nginx.conf

--- misc/ec2-startup.sh
+++ misc/ec2-startup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# NOTE: Add "/home/ubuntu/fastcgi-blog/misc/ec2-startup.sh" line to /etc/rc.local on ec2 ubuntu instance so that fcgi app automatically runs each time ec2 instance is restarted
+
 sudo apt update
 sudo apt upgrade
 sudo apt install nginx libfcgi-dev g++
 rm -rf fastcgi-blog
 git clone https://github.com/mmyoungman/fastcgi-blog.git
+sudo cp fastcgi-blog/misc/nginx.conf /etc/nginx/
+sudo systemctl restart nginx
 cd fastcgi-blog/code
 ./build.sh
 cd ../build
